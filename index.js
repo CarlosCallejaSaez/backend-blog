9ea204e6db51bdabafdbf9cd98c7e6c07812dcd1
Carlos Calleja Saez <carloscallejasez@gmail.com>
2023-11-04 18:39:49 +0100

    update done

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
-const express=require('express')
+const express = require('express')
 const app = express()
 const dotenv = require('dotenv')
 const mongoose = require('mongoose')
-const authRoute=require('./routes/auth.js')
+const authRoute = require('./routes/auth.js')
+const userRoute = require("./routes/users")
 
 dotenv.config()
 app.use(express.json())
 
 mongoose.connect(process.env.MONGO_URL)
-.then(()=>console.log('Connected to Mongo'))
-.catch((err)=>console.log(err)
-)
+    .then(() => console.log('Connected to Mongo'))
+    .catch((err) => console.log(err)
+    )
 
 
-app.use('/api/auth',authRoute)
+app.use('/api/auth', authRoute)
+app.use("/api/users", userRoute)
 
-const PORT=5000
 
-app.listen(PORT,()=>console.log(`Servidor corriendo en el puerto ${PORT}`))
+const PORT = 5000
+
+app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`))
